738664c01e25b0784c514d8b4c194c5db309952a
Marek Jaczewski <jaczewski147@gmail.com>
2020-10-29 20:26:08 +0100

    correct face-down-cards component styles

--- src/app/components/face-down-cards/face-down-cards.component.ts
+++ src/app/components/face-down-cards/face-down-cards.component.ts
@@ -1,53 +1,61 @@
 import { Component, ElementRef, HostBinding, Input, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-face-down-cards',
-  templateUrl: './face-down-cards.component.html',
-  styleUrls: ['./face-down-cards.component.less']
+	selector: 'app-face-down-cards',
+	templateUrl: './face-down-cards.component.html',
+	styleUrls: ['./face-down-cards.component.less'],
 })
 export class FaceDownCardsComponent implements OnInit {
-  @HostBinding('style') style;
-  @Input() set numberOfCards(numberOfCards: number) {
-    this.numbers = Array(numberOfCards).fill(0);
-  }
-  @Input() cardOverlap = 1;
-  @Input() set direction(direction: string) {
-    if (direction === 'vertical') {
-      this.style = this._sanitizer.bypassSecurityTrustStyle('display: block; transform-origin: top left; transform: rotate(-90deg) translate(-100%);');
-    }
-  }
-  numbers: number[];
-  maxCardsSpace: number;
-  cardWidth = 200;
+	@HostBinding('style') style: SafeStyle;
+	@Input() set numberOfCards(numberOfCards: number) {
+		this.numbers = Array(numberOfCards).fill(0);
+	}
+	@Input() cardOverlap: number = 1;
+	@Input() maxCardsSpace: number = 500;
+	@Input() set rotation(rotation: number) {
+		this.style = this._sanitizer.bypassSecurityTrustStyle(
+			`display: block; transform-origin: bottom right; transform: rotate(${rotation}deg) translate(-${
+				this.cardsSpaceWidth > this.maxCardsSpace ? this.maxCardsSpace / 2 : this.cardsSpaceWidth / 2
+			}px);`,
+		);
+	}
+	numbers: number[];
+	cardWidth: number = 200;
 
-  constructor(private _element: ElementRef, private _sanitizer: DomSanitizer) { }
+	constructor(private _element: ElementRef, private _sanitizer: DomSanitizer) {}
 
-  ngOnInit(): void {
-    this.maxCardsSpace = this.direction === 'vertical' ? this._element.nativeElement.clientHeight : this._element.nativeElement.clientWidth;
-    if (this.cardsSpaceWidth > this.maxCardsSpace){
-      this.adjustCardOverlap();
-    }
-  }
+	ngOnInit(): void {
+		// this.maxCardsSpace = this.rotation === 90 ? this._element.nativeElement.clientHeight : this._element.nativeElement.clientWidth;
+		if (this.cardsSpaceWidth > this.maxCardsSpace) {
+			console.log('card space width: ' + this.cardsSpaceWidth);
+			console.log('max cards space: ' + this.maxCardsSpace);
 
-  getMoveFromLeft(idx: number): string {
-      return `${idx * this.cardOverlap}px`;
-  }
+			this.adjustCardOverlap();
+		}
+	}
 
-  get cardsSpaceWidth(): number{
-      return this.cardWidth + (this.numbers.length - 1) * this.cardOverlap;
-  }
+	getMoveFromLeft(idx: number): string {
+		return `${idx * this.cardOverlap}px`;
+	}
 
-  adjustCardOverlap(): void {
-    const outSideCardSpace = this.cardsSpaceWidth - this.maxCardsSpace;
-    const cardMoveCorrection = outSideCardSpace / (this.numbers.length - 1);
-    this.cardOverlap = this.cardOverlap - cardMoveCorrection;
-  }
+	get cardsSpaceWidth(): number {
+		return this.cardWidth + (this.numbers.length - 1) * this.cardOverlap;
+	}
 
-  getdirection(): string {
-    if (this.direction !== 'horizontal') {
-      return 'left';
-    }
-    return 'top';
-  }
+	adjustCardOverlap(): void {
+		const outSideCardSpace = this.cardsSpaceWidth - this.maxCardsSpace;
+		console.log(this.cardsSpaceWidth);
+		const cardMoveCorrection = outSideCardSpace / (this.numbers.length - 1);
+		console.log(outSideCardSpace);
+
+		this.cardOverlap = this.cardOverlap - cardMoveCorrection;
+	}
+
+	getdirection(): string {
+		if (this.rotation !== 0) {
+			return 'left';
+		}
+		return 'top';
+	}
 }
